4c2ba7924a22ff33dd2ce90e9f2a331583a6f480
ahmet <ahmet55uluisik05@gmail.com>
2023-05-09 11:37:47 +0300

    created new components

---
+++ src/components/dice/Roll.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Roll.css";
+const Roll = () => {
+  return <div>Roll</div>;
+};
+
+export default Roll;
---
+++ src/components/rolldice/RollDice.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./RollDice.css";
+const RollDice = () => {
+  return <div>RollDice</div>;
+};
+
+export default RollDice;
